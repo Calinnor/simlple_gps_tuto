f4dccc67515b5866b8c129713559913ca65d3195
Calinnor <58951773+Calinnor@users.noreply.github.com>
2021-06-12 13:25:21 +0200

    Commit on third branch : implement dedied class to fetch location

---
+++ app/src/main/java/com/cirederf/mygpstuto/GPS_Receiver.java
@@ -0,0 +1,102 @@
+package com.cirederf.mygpstuto;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+
+//step 1 implement LocationListerner
+public class GPS_Receiver implements LocationListener {
+
+    //step 2 add Context and constructor
+    Context context;
+
+    public GPS_Receiver(Context context) {
+        this.context = context;
+    }
+
+    //step 4 create a method to get location
+    public Location getLocation() {
+
+        //step 5: adding requestoptions
+        //with ContextCompat
+        if (ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(context, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Toast.makeText(context, "Permissions not granted ! Apply manually in app setting of your device", Toast.LENGTH_SHORT).show();
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return null;
+        }
+
+        //with ActivityCompat
+//        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+//        ActivityCompat.checkSelfPermission(context, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+//            Toast.makeText(context, "Permissions not granted !", Toast.LENGTH_SHORT).show();
+//            // TODO: Consider calling
+//            //    ActivityCompat#requestPermissions
+//            // here to request the missing permissions, and then overriding
+//            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//            //                                          int[] grantResults)
+//            // to handle the case where the user grants the permission. See the documentation
+//            // for ActivityCompat#requestPermissions for more details.
+//            return null;
+//        }
+
+
+        //if(ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION))
+        // step 4.1 get systemService via context
+        LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        //step 4.2 check if gps is enable
+        boolean isGpsEnable = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+        if (isGpsEnable) {
+            //step 4.5 create request to lacation
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, this);
+            //step 6:
+            Location locationToReturn = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            return locationToReturn;
+            //step 5 add the request options ahead
+
+        } else {
+            //step 4.3
+            Toast.makeText(context, "Please, activate GPS !", Toast.LENGTH_SHORT).show();
+        }
+
+        //Location getLocation() return...in other case
+        return null;
+
+
+    }
+
+
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+
+    }
+//step 3 implement listerner methods
+}
--- app/src/main/java/com/cirederf/mygpstuto/MainActivity.java
+++ app/src/main/java/com/cirederf/mygpstuto/MainActivity.java
@@ -1,23 +1,16 @@
 package com.cirederf.mygpstuto;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements LocationListener {
+public class MainActivity extends AppCompatActivity {
 
-    private static final int REQUEST_CODE_PERMISSIONS = 12340;
-    private LocationManager locationManager;
     private Button buttonGetLocation;
     private TextView textLongitude;
     private TextView textLatitude;
@@ -30,122 +23,23 @@ public class MainActivity extends AppCompatActivity implements LocationListener 
         buttonGetLocation = findViewById(R.id.button_getting_location);
         textLatitude = findViewById(R.id.latitude);
         textLongitude = findViewById(R.id.longitude);
+        onClickButton();
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        checkForLocationPermissions();
-
-        //step 1 create a pop up permission allowed (line 42 to 53)
-        // which will be placed in a dedied method below with all others informations (42 to 69)
-
-//        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-//            && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-//            //step 1.1 add the created pop up "permission allowed"
-//            ActivityCompat.requestPermissions(
-//                    this,
-//                    new String[] {
-//                    Manifest.permission.ACCESS_FINE_LOCATION,
-//                    Manifest.permission.ACCESS_COARSE_LOCATION
-//                    },
-//                    REQUEST_CODE_PERMISSIONS
-//            );
-//            //step 2: get all informations in a dedied method :line 42 to 68 !
-//            //go in private void checkForPermissions()
-//            return;
-//        }
-//
-//        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-//        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-//
-//            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, this);
-//        }
-//        if (locationManager.isProviderEnabled(LocationManager.PASSIVE_PROVIDER)) {
-//            locationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 5000, 10, this);
-//        }
-//        if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
-//            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 10, this);
-//        }
-    }
-
-    //step 2 the dedied method to make the call in async
-    private void checkForLocationPermissions() {
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
-                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(
-                    this,
-                    new String[] {
-                            Manifest.permission.ACCESS_FINE_LOCATION,
-                            Manifest.permission.ACCESS_COARSE_LOCATION
-                    },
-                    REQUEST_CODE_PERMISSIONS
-            );
-            return;
-        }
-
-        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
-        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
-
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, this);
-        }
-        if (locationManager.isProviderEnabled(LocationManager.PASSIVE_PROVIDER)) {
-            locationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 5000, 10, this);
-        }
-        if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
-            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 10, this);
-        }
-        //step 3 go in onRequestPermissionsResult
-    }
-
-    //step 3 : override onRequestPermissionsResult
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        //step 3.1 create a loop for seeking permissions allowed: if permission allowed: check, if not : popup
-        if(requestCode == REQUEST_CODE_PERMISSIONS) {
-            checkForLocationPermissions();
-        }
-        //End of the tuto for simple gps implementation.
-        //note: if you use a emulator you have false gps location given with the system
-        //note 2 : after update this app with those code lines
-        // , you have to modify the allowed permissions in the app system in the device to test correctly the update...;)
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if(locationManager != null) {
-            locationManager.removeUpdates(this);
-        }
-    }
-
-    @Override
-    public void onLocationChanged(@NonNull Location location) {
-        double latitude = location.getLatitude();
-        double longitude = location.getLongitude();
-
+    private void onClickButton() {
         buttonGetLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                textLatitude.setText(String.valueOf(latitude));
-                textLongitude.setText(String.valueOf(longitude));
+                GPS_Receiver gps = new GPS_Receiver(getApplicationContext());
+                Location location = gps.getLocation();
+                if (location != null) {
+                    double lat = location.getLatitude();
+                    double longi = location.getLongitude();
+                    textLatitude.setText(String.valueOf(lat));
+                    textLongitude.setText(String.valueOf(longi));
+                    Toast.makeText(getApplicationContext(), "Latitude ="+lat+" // Longitude ="+longi, Toast.LENGTH_LONG).show();
+                }
             }
         });
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-
-    }
-
-    @Override
-    public void onProviderEnabled(@NonNull String provider) {
-
-    }
-
-    @Override
-    public void onProviderDisabled(@NonNull String provider) {
-
     }
 }
