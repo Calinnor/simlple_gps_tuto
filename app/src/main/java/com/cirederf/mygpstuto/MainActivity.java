734f8ae899240f2dcc164d06d4e29ee232a0939a
Calinnor <58951773+Calinnor@users.noreply.github.com>
2021-06-14 22:57:26 +0200

    Adding key works well on step 7

--- app/src/main/java/com/cirederf/mygpstuto/MainActivity.java
+++ app/src/main/java/com/cirederf/mygpstuto/MainActivity.java
@@ -1,73 +1,97 @@
 package com.cirederf.mygpstuto;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 
 import android.Manifest;
-import android.content.Intent;
+import android.app.FragmentManager;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.ResultReceiver;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
-import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements LocationListener {
+
+    //map implementatiion: step 1 in gradle
+    //step 2 modify xml
+    //step 3 modify attributs and on clicklisterner
+    private static final int REQUEST_CODE_PERMISSIONS = 12340;
+    private LocationManager locationManager;
+    private MapFragment mapFragment;
+    private GoogleMap googleMap;
 
 
-public class MainActivity extends AppCompatActivity {
-
-    private static final int REQUEST_CODE_LOCATION_PERMISSIONS = 12340;
-
-    private Button buttonGetLocation;
-    private TextView textCurrentLocation, textAddress;
-    private ProgressBar progressBar;
-
-    //step 10
-    private ResultReceiver resultReceiver;
-    //step 11 initiate resultReceiver (here in main)
-
-    //step 1 create a class for fetch address
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //step 11
-        resultReceiver = new AdressesResultReceiver(new Handler());
-        //step 12 create a method to fetchAdress
-
-        textCurrentLocation = findViewById(R.id.current_location);
-        textAddress = findViewById(R.id.textAdress);
-        buttonGetLocation = findViewById(R.id.button_get_current_location);
-        progressBar = findViewById(R.id.progressBar);
-        onButtonLocationClick();
+        //step 4 initiate fragment and cast to MapFragment
+        FragmentManager fragmentManager = getFragmentManager();
+        mapFragment = (MapFragment) fragmentManager.findFragmentById(R.id.id_for_google_map_fragment);
+        //step 5 create a method to getGoogleMap
     }
 
-    //step 12
-    private void fetchAddressFromCoordinates(Location location) {
-        Intent intent = new Intent(this, FetchAddressWithFusedLocation.class);
-        intent.putExtra(Constants.RECEIVER, resultReceiver);
-        intent.putExtra(Constants.LOCATION_DATA_EXTRAS, location);
-        startService(intent);
-    }//step 13 in getCurrentLocation()
+    @Override
+    protected void onResume() {
+        super.onResume();
+        checkForLocationPermissions();
+    }
 
-    private void onButtonLocationClick() {
-        buttonGetLocation.setOnClickListener(new View.OnClickListener() {
+    private void checkForLocationPermissions() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[] {
+                            Manifest.permission.ACCESS_FINE_LOCATION,
+                            Manifest.permission.ACCESS_COARSE_LOCATION
+                    },
+                    REQUEST_CODE_PERMISSIONS
+            );
+            return;
+        }
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000, 10, this);
+        }
+        if (locationManager.isProviderEnabled(LocationManager.PASSIVE_PROVIDER)) {
+            locationManager.requestLocationUpdates(LocationManager.PASSIVE_PROVIDER, 5000, 10, this);
+        }
+        if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 5000, 10, this);
+        }
+        getMyGoogleMap();
+        //step 7 modify onLocationChanged
+    }
+
+    //step 5 :method to call google map in async
+    //step 5.3 add suppress warning
+    @SuppressWarnings("MissingPermission")
+    private void getMyGoogleMap() {
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
-            public void onClick(View v) {
-                    getCurrentLocation();
+            public void onMapReady(GoogleMap googleMap) {
+                //5.1 implement support_view (mainactivity) for map
+                MainActivity.this.googleMap = googleMap;
+                googleMap.moveCamera(CameraUpdateFactory.zoomBy(15));
+                //step 5.2 not needing new permission check because this method will be call just after the permissions call already done
+                googleMap.setMyLocationEnabled(true);
+                googleMap.addMarker(new MarkerOptions().position(new LatLng(48.5277772, 2.6580556))
+                        .title("apm"));
+                //step 6 after checkpermissions call method
+
             }
         });
     }
@@ -75,102 +99,46 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == REQUEST_CODE_LOCATION_PERMISSIONS && grantResults.length > 0) {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                getCurrentLocation();
-            } else {
-                Toast.makeText(this, "Need permissions", Toast.LENGTH_LONG).show();
-            }
+        if(requestCode == REQUEST_CODE_PERMISSIONS) {
+            checkForLocationPermissions();
         }
     }
 
-    private void getCurrentLocation() {
-        progressBar.setVisibility(View.VISIBLE);
-
-        LocationRequest locationRequest = new LocationRequest();
-        locationRequest.setInterval(5000);
-        locationRequest.setFastestInterval(2500);
-        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            if (ContextCompat.checkSelfPermission(getApplicationContext()
-                    , Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(
-                        MainActivity.this
-                        , new String[]{
-                                Manifest.permission.ACCESS_FINE_LOCATION
-                        }
-                        , REQUEST_CODE_LOCATION_PERMISSIONS
-                );
-            }
-            return;
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(locationManager != null) {
+            locationManager.removeUpdates(this);
         }
-
-        LocationServices.getFusedLocationProviderClient(MainActivity.this)
-                .requestLocationUpdates(locationRequest, new LocationCallback() {
-
-                    @Override
-                    public void onLocationResult(LocationResult locationResult) {
-                        super.onLocationResult(locationResult);
-
-                       LocationServices.getFusedLocationProviderClient(MainActivity.this)
-                                .removeLocationUpdates(this);
-
-                        if(locationResult != null && locationResult.getLocations().size() > 0) {
-
-                            int latestLocationIndex = locationResult.getLocations().size() -1;
-                            
-                            double latitude = locationResult.getLocations().get(latestLocationIndex).getLatitude();
-                            double longitude = locationResult.getLocations().get(latestLocationIndex).getLongitude();
-                            textCurrentLocation.setText(
-                                    String.format(
-                                            "Latitude: %s\nLongitude: %s ",
-                                            latitude, longitude
-                                    )
-                            );
-                            //step 14
-                            Location location = new Location("providerNA");
-                            location.setLatitude(latitude);
-                            location.setLongitude(longitude);
-                            fetchAddressFromCoordinates(location);
-                          //step 13 placing here progressBar
-                        }else {
-                            progressBar.setVisibility(View.GONE);
-                        }
-                        //in step 13 progressBar move just ahead
-//                        progressBar.setVisibility(View.GONE);
-
-                    }
-                }
-                , Looper.getMainLooper());
     }
 
-    //step 9 create class which receive results for addresses
-    private class AdressesResultReceiver extends ResultReceiver {
+    @Override
+    public void onLocationChanged(@NonNull Location location) {
+        double latitude = location.getLatitude();
+        double longitude = location.getLongitude();
 
-        //step 9.1 constructor without public
-        /**
-         * Create a new ResultReceive to receive results.  Your
-         * {@link #onReceiveResult} method will be called from the thread running
-         * <var>handler</var> if given, or from an arbitrary thread if null.
-         *
-         * @param handler
-         */
-        AdressesResultReceiver(Handler handler) {
-            super(handler);
+        Toast.makeText(this, "Location "+latitude +"/"+ longitude, Toast.LENGTH_LONG).show();
+        //step 7.1 if map is loaded (not null)
+        if (googleMap != null) {
+            LatLng googleLocation = new LatLng(latitude, longitude);
+            googleMap.moveCamera(CameraUpdateFactory.newLatLng(googleLocation));
+            //step 8 in manifest add apiKey
         }
 
-        //step 9.2 onReceiverResult
-        @Override
-        protected void onReceiveResult(int resultCode, Bundle resultData) {
-            super.onReceiveResult(resultCode, resultData);
-            if (resultCode == Constants.SUCCES_RESULT) {
-                textAddress.setText(resultData.getString(Constants.RESULT_DATA_KEY));
-            }else {
-                Toast.makeText(MainActivity.this, resultData.getString(Constants.RESULT_DATA_KEY), Toast.LENGTH_SHORT).show();
-            }
-            progressBar.setVisibility(View.GONE);
-            //step 10 declaring ResultReceiver atribut
-        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(@NonNull String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(@NonNull String provider) {
+
     }
 }
